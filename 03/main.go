bd42946a9a803c9bdb27b4fde9c8cb3678abc545
Gerard <gerard@wetransfer.com>
2023-08-10 15:24:23 +0200

    Add 3p2

--- 03/main.go
+++ 03/main.go
@@ -6,20 +6,45 @@ import (
 	"os"
 )
 
-func main() {
-	directions, err := os.ReadFile("input")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Amount of houses that get at least 1 present: %d\n", houses(directions))
-}
-
 type point struct {
 	x int
 	y int
 }
 
-func houses(directions []byte) int {
+func main() {
+	directions, err := os.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Amount of houses that get at least 1 present: %d\n", routeWithSanta(directions))
+	fmt.Printf("Amount of houses with Robo Santa that get at least 1 present: %d\n", routeWithRobo(directions))
+}
+
+func routeWithSanta(directions []byte) int {
+	return len(route(directions))
+}
+
+func routeWithRobo(directions []byte) int {
+	dirsSanta := []byte{}
+	dirsRobo := []byte{}
+
+	for i, d := range directions {
+		if i%2 == 0 {
+			dirsSanta = append(dirsSanta, d)
+		} else {
+			dirsRobo = append(dirsRobo, d)
+		}
+	}
+
+	santaMap := route(dirsSanta)
+	for k, v := range route(dirsRobo) {
+		santaMap[k] += v
+	}
+	return len(santaMap)
+}
+
+func route(directions []byte) map[point]int {
 	start := point{0, 0}
 	counts := make(map[point]int)
 	counts[start] += 1
@@ -43,5 +68,5 @@ func houses(directions []byte) int {
 		counts[start] += 1
 	}
 
-	return len(counts)
+	return counts
 }
