a59f6cb9b4d4afd28288a547df543c7fdb1294cd
Gerard <gerard@wetransfer.com>
2023-08-15 11:57:07 +0200

    Some naming

--- 04/main.go
+++ 04/main.go
@@ -9,18 +9,18 @@ import (
 const INPUT string = "ckczppom"
 
 func main() {
-	fmt.Println("Part 1:", p1(INPUT, "00000"))
-	fmt.Println("Part 2:", p1(INPUT, "000000"))
+	fmt.Println("Part 1:", calculateMd5(INPUT, "00000"))
+	fmt.Println("Part 2:", calculateMd5(INPUT, "000000"))
 }
 
-func p1(input string, end string) int {
+func calculateMd5(input string, prefix string) int {
 	start := 1
 
 	for {
 		comb := fmt.Sprintf("%s%d", input, start)
 		result := md5.Sum([]byte(comb))
 
-		if strings.HasPrefix(fmt.Sprintf("%x", result), end) {
+		if strings.HasPrefix(fmt.Sprintf("%x", result), prefix) {
 			return start
 		}
 
--- 04/main_test.go
+++ 04/main_test.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPartOneEx1(t *testing.T) {
-	result := p1("abcdef")
-	fmt.Println(result)
+	result := calculateMd5("abcdef", "00000")
 	if result != 609043 {
 		t.Fatal("Does not match")
 	}
 }
 
 func TestPartOneEx2(t *testing.T) {
-	result := p1("pqrstuv")
-	fmt.Println(result)
+	result := calculateMd5("pqrstuv", "00000")
 	if result != 1048970 {
 		t.Fatal("Does not match")
 	}
