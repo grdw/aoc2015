c819ef342d0e7f3e0f9d2815e739026e342de88a
Gerard <gerard@wetransfer.com>
2023-08-22 14:56:33 +0200

    Add part 2

--- 13/main.go
+++ 13/main.go
@@ -4,14 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 type calcHeapFunc = func(a []string, hi map[string]int) int
 
 func main() {
 	guests, happinessIndex := parse("input")
+	fmt.Println("Part 1:", maxHappiness(guests, happinessIndex))
+
+	me := "me"
+	for _, guest := range guests {
+		happinessIndex[fmt.Sprintf("%s-%s", guest, me)] = 0
+		happinessIndex[fmt.Sprintf("%s-%s", me, guest)] = 0
+	}
+	guests = append(guests, me)
+	fmt.Println("Part 2:", maxHappiness(guests, happinessIndex))
+}
+
+func maxHappiness(guests []string, happinessIndex map[string]int) int {
 	max := 0
 	heapPermutation(
 		guests,
@@ -20,11 +34,12 @@ func main() {
 		calculateHappiness,
 		&max,
 	)
-	fmt.Println("Part 1:", max)
+	return max
 }
 
 func calculateHappiness(guests []string, happinessIndex map[string]int) int {
 	total := 0
+
 	for i := 0; i < len(guests); i++ {
 		nextIndex := (i + 1) % len(guests)
 		lr := fmt.Sprintf("%s-%s", guests[i], guests[nextIndex])
@@ -61,8 +76,8 @@ func heapPermutation(
 
 func parse(file string) ([]string, map[string]int) {
 	guests := []string{}
-	guestMap := make(map[string]bool)
 	happinessIndex := make(map[string]int)
+
 	readFile, err := os.Open(file)
 	defer readFile.Close()
 
@@ -74,25 +89,20 @@ func parse(file string) ([]string, map[string]int) {
 	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
-		line := fileScanner.Bytes()
-		re := regexp.MustCompile(
-			`([A-Za-z]+) would (lose|gain) (\d+) happiness units by sitting next to ([A-Za-z]+)`,
-		)
+		line := fileScanner.Text()
+		words := strings.Split(line[0:len(line)-1], " ")
 
-		matches := re.FindAllSubmatch(line, -1)
-		p1 := string(matches[0][1])
-		lossGain := string(matches[0][2])
-		hap, _ := strconv.Atoi(string(matches[0][3]))
-		p2 := string(matches[0][4])
-		if lossGain == "lose" {
+		p1 := words[0]
+		p2 := words[len(words)-1]
+		hap, _ := strconv.Atoi(words[3])
+		if words[2] == "lose" {
 			hap *= -1
 		}
-		happinessIndex[fmt.Sprintf("%s-%s", p1, p2)] = hap
-		guestMap[p1] = true
-	}
 
-	for guest, _ := range guestMap {
-		guests = append(guests, guest)
+		happinessIndex[fmt.Sprintf("%s-%s", p1, p2)] = hap
+		if !slices.Contains(guests, p1) {
+			guests = append(guests, p1)
+		}
 	}
 
 	return guests, happinessIndex
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require golang.org/x/crypto v0.12.0
 
 require (
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/term v0.11.0 // indirect
 )
