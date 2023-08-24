115d86ba1ad1a1fd709bc30b9482ac05592cea09
Gerard <gerard@wetransfer.com>
2023-08-24 13:32:59 +0200

    Resolve part 2

--- 18/main.go
+++ 18/main.go
@@ -14,14 +14,25 @@ type coord struct {
 
 func main() {
 	exGrid := parse("input_test", 6)
-	fmt.Println("Ex 1:", animate(exGrid, 4))
+	fmt.Println("Ex 1:", animate(exGrid, 4, false))
+	exGrid = parse("input_test", 6)
+	fmt.Println("Ex 2:", animate(exGrid, 5, true))
 	grid := parse("input", 100)
-	fmt.Println("Part 1:", animate(grid, 100))
+	fmt.Println("Part 1:", animate(grid, 100, false))
+	grid = parse("input", 100)
+	fmt.Println("Part 2:", animate(grid, 100, true))
 }
 
-func animate(grid [][]int, cycles int) int {
+func animate(grid [][]int, cycles int, locked bool) int {
 	gridSize := len(grid)
 	neighborsGrid := make([][]int, gridSize)
+
+	if locked {
+		grid[0][0] = 1
+		grid[0][gridSize-1] = 1
+		grid[gridSize-1][0] = 1
+		grid[gridSize-1][gridSize-1] = 1
+	}
 
 	for i := 0; i < cycles; i++ {
 		for y := 0; y < gridSize; y++ {
@@ -59,6 +70,12 @@ func animate(grid [][]int, cycles int) int {
 
 		for y := 0; y < gridSize; y++ {
 			for x := 0; x < gridSize; x++ {
+				if locked && ((y == 0 && x == 0) ||
+					(y == 0 && x == gridSize-1) ||
+					(y == gridSize-1 && x == 0) ||
+					(y == gridSize-1 && x == gridSize-1)) {
+					continue
+				}
 				if grid[y][x] == 1 &&
 					!(neighborsGrid[y][x] == 2 ||
 						neighborsGrid[y][x] == 3) {
