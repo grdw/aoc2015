4e2b7273068954d0729b50506ce3b517f1cbdf3f
Gerard <gerard@wetransfer.com>
2023-08-28 17:08:09 +0200

    Brute forced part 2 ...

--- 20/main.go
+++ 20/main.go
@@ -9,25 +9,49 @@ const input int = 36000000
 func main() {
 	p1 := gift()
 	fmt.Println("part 1:", p1)
+	p2 := giftWithLimit()
+	fmt.Println("part 2:", p2)
 }
 
 func gift() int {
 	houseIndex := 0
-	incr := 10000
+	incr := 100
 
 	for {
 		total := presents(houseIndex)
 
 		if total > input {
-			if incr == 1 {
-				return houseIndex
-			}
-			houseIndex -= incr * 100
-			incr /= 10
-
+			return houseIndex
 		}
 		houseIndex += incr
 	}
+}
+
+// TODO: This can be done better I guess...
+// .. but oh whelp, not a math genius... so brute force it is!
+func giftWithLimit() int {
+	houseIndex := 0
+	incr := 10
+
+	for {
+		total := presentsWithLimit(houseIndex)
+
+		if total > input {
+			return houseIndex
+		}
+		houseIndex += incr
+	}
+}
+
+func presentsWithLimit(i int) int {
+	total := 0
+	limit := 50
+	for j := 1; j <= i; j++ {
+		if i%j == 0 && j*limit >= i {
+			total += j * 11
+		}
+	}
+	return total
 }
 
 func presents(i int) int {
